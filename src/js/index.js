18a4a433fd0ecd5ee30fd2d790e75305f4fd82a2
mfuent <mauriciofb@gmail.com>
2026-02-09 17:39:31 -0300

    first JS lesson init

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+console.log("Hello from console")
+
+// tipos JS
+// Strings -> texto
+edad = "treinta años" // sin palabra clave
+var nombre = "Gato Juanito" // palabra clave var
+let dispositivo = "Teléfono Android" // palabra clave let
+const isbn = "12039 - 123- 123- 123- 123" // palabra clave const
